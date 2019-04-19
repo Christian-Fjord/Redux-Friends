836235aa3663b483a5977294f0c128a0d42603c6
Christian-Fjord <fjord.christian@gmail.com>
2019-04-18 21:56:03 -0500

    Added friends and both forms to App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Friends from './components/Friends.js';
+import CreateFriendForm from './components/CreateFriendForm.js';
+import UpdateFriendForm from './components/UpdateFriendForm.js';
+
 import './App.css';
 
 class App extends Component {
@@ -7,6 +10,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1> Friends App </h1>
+        <Friends />
+        <CreateFriendForm />
+        <UpdateFriendForm />
       </div>
     );
   }
