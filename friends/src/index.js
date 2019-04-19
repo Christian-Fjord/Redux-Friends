a34b7239e394ba995e5ab12678e039765dd6f0f0
Christian-Fjord <fjord.christian@gmail.com>
2019-04-18 21:20:28 -0500

    Initialized src/index.js

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 import './index.css';
 
+const store = createStore(
+  rootReducer, 
+  applyMiddleware(thunk, logger)
+)
 
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider> 
+, document.getElementById('root'));
